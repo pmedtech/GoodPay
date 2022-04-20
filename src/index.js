3b719a96b6fb2b58b6dd65981902eb0eef5d42c5
Pâmela Cristina <pamp07@hotmail.com>
2022-04-20 12:03:33 -0300

    Banner com textos atualizados e Carrossel.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 var janelaLogin = new bootstrap.Modal(document.getElementById("loginNegado"));
+var janelaPromocao = new bootstrap.Modal(document.getElementById("promo1"));
 
 function rotaCadastro() {
     window.location.href = "../registro.html";
@@ -14,5 +15,17 @@ function rotaLogin() {
     } else {
         janelaLogin.show();
     }
+}
+function alertaPromo() {
+    let titulo = "Promoção 1 [Quebra da Banca]";
+    let corpo = "Super legal <br> <img src='./img/4.png' width='160' height='160'>";
+   // let lista = "<ul class='list-group' style='margin-top: 15px'>"
+   // for(let i = 0; i < 10; i++) {
+   //     lista +=`<li class='list-group-item'>(${(i * 5)})</li>`;
+    //}
+    //lista +="</ul>";
+    janelaPromocao.show();
 
+    document.getElementById("titulo").innerHTML = titulo;
+    document.getElementById("corpo").innerHTML = corpo//+lista;
 }
